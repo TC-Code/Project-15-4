fbf82b8e12d03c6a93ec77470cd0ec279f726b44
Tomasz Chowaniec <tomasz.chowaniec@icloud.com>
2018-04-25 23:45:43 +0200

    Add initialValue to reduce() method

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-// Zadanie 1.
+// Exercise 1.
 const a = "Hello";
 const b = "World";
 function hello() {
-    console.log(`${a} ${b}`)
-};
+  console.log(`${a} ${b}`);
+}
 hello();
 
-// Zadanie 2.
+// Exercise 2.
 const multiply = (x = 1, y = 1) => x * y;
 console.log(multiply(4));
 
-//Zadanie 3.
-const average = (...args) => console.log(args.reduce((c, d) => c + d)/args.length);
-average(24, 3, 33, 4, 45);
+// Exercise 3.
+const average = (...args) =>
+  args.reduce((c, d) => {
+    return c + d / args.length;
+  }, 0);
+average(12, 2, 9, 77);
 
-// Zadanie 4.
+// Exercise 4.
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 average(...grades);
 
-// Zadanie 5.
-const array = [1, 4, 'Iwona', false, 'Nowak'];
-const [, ,firstName, , lastName] = array;
+// Exercise 5.
+const array = [1, 4, "Iwona", false, "Nowak"];
+const [, , firstName, , lastName] = array;
 console.log(`${firstName} ${lastName}`);
